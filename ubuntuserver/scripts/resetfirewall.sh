a3620fc7a3155e89c4e36ff5f79d3e10b32933e9
tjelite1986 <113060877+tjelite1986@users.noreply.github.com>
2024-08-26 14:08:10 +0200

    Update resetfirewall.sh

--- ubuntuserver/scripts/resetfirewall.sh
+++ ubuntuserver/scripts/resetfirewall.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-
+# Step 1: Disabling ufw
 sudo ufw --force disable \
+# Step 2: Resetting ufw
 && sudo ufw --force reset \
+# Step 3: Deny All Incoming Connections
 && sudo ufw default deny incoming \
+# Step 4: Allow All Outgoing Connections
 && sudo ufw default allow outgoing \
+#Step 5: Defining Ports That Allow Connections
 && sudo ufw allow 22/tcp \
 && sudo ufw allow 80/tcp \
 && sudo ufw allow 443/tcp \
-&& sudo ufw --force enable
+# Step 6: Enabling ufw
+&& sudo ufw --force enable #\
+# Additional Ports
+# MongoDB
+# && sudo ufw allow 27017/tcp \
+# MySQL
+# && sudo ufw allow 3306/tcp \
+# Postgres
+# && sudo ufw allow 5432/tcp \
+# Redis
+# && sudo ufw allow 6379/tcp
 
 # How to use it:
 # Rename the file If you want
